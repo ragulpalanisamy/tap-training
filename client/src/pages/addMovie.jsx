b7f0df3cc0839ea411bc0388c7a706889960536e
ragulpalanisamy <ragul101001@gmail.com>
2023-05-14 17:25:35 +0530

    added react bootstrap template in add movie page

--- client/src/pages/addMovie.jsx
+++ client/src/pages/addMovie.jsx
@@ -1,17 +1,58 @@
 import { Outlet } from "react-router-dom";
+import { Form, Button, Card } from "react-bootstrap";
+import { useState } from "react";
 
 function AddMovie() {
-    const buildAddMovieForm = () => {
-      return <h1>Add movie to the list</h1>;
+  const AddMovieForm = () => {
+    const [formValues, setFormValues] = useState({});
+
+    const onChangeFormField = (event) => {
+      const { value, name, type } = event.target;
+
+      setFormValues({
+        ...formValues,
+        [name]: type === "number" ? Number(value) : value,
+      });
     };
-  
+
+    const onClickSubmit = () => {
+      console.log(formValues);
+    };
+
     return (
-      <div>
-        <Outlet />
-        {buildAddMovieForm()}
-      </div>
+      <Card className="mt-5 mx-5">
+        <Card.Header className="text-center">
+          <h4>ADD MOVIES</h4>
+        </Card.Header>
+        <Card.Body>
+          <Form.Group className="mb-3" controlId="title">
+            <Form.Label>Movie Title</Form.Label>
+            <Form.Control type="text" name="title" onChange={onChangeFormField} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="rating">
+            <Form.Label>Movie Rating</Form.Label>
+            <Form.Control type="number" name="rating" onChange={onChangeFormField} />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="poster">
+            <Form.Label>Movie Poster</Form.Label>
+            <Form.Control type="text" name="poster" onChange={onChangeFormField} />
+          </Form.Group>
+          <Button variant="primary" type="button" onClick={onClickSubmit}>
+            Add Movie
+          </Button>
+        </Card.Body>
+      </Card>
     );
-  }
-  
+  };
+
+  return (
+    <div>
+      <Outlet />
+      <AddMovieForm />
+    </div>
+  );
+}
 
 export default AddMovie;
